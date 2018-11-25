ea9be55b3d0db718d2abe6ec8388082a4de3c8bb
SamSamhuns <sthapuskar@yahoo.com>
2018-11-25 14:55:06 -0500

    Ran atom-beautify for auto-linting

--- Discounted Cash Flow/main_DCF.py
+++ Discounted Cash Flow/main_DCF.py
@@ -1,5 +1,8 @@
 # coding: utf-8
 
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
 '''
 Utility Python program for calculating Net Present Value of cash flows over a yearly period
 DCF Calculations for yearly cash flows
@@ -11,15 +14,12 @@ discount rate has to be entered separately.
 # Using Python 3
 import matplotlib as mpl
 mpl.use('Agg')
-import sys
-import numpy as np
-import matplotlib.pyplot as plt
 
 
 # Function to calculate present value of cash flow
 # rate is in % per year and time is in years
-def presentVal ( futureVal, rate, time ):
-    return futureVal / ((1+(rate/100))**time)
+def presentVal(futureVal, rate, time):
+    return futureVal / ((1 + (rate / 100))**time)
 
 #  CSV file format
 #  Note that current year is stated as 0
@@ -28,19 +28,22 @@ def presentVal ( futureVal, rate, time ):
 #  1,    1500
 #  2,    1600
 
+
 def main():
     if len(sys.argv) != 3:
         print("Usage: python main_DCF.py <DISCOUNT RATE> <CSV FILE>")
         sys.exit()
     # Error check
     try:
-        fp = open( sys.argv[2], 'r')
-        d_rate = float(sys.argv[1])       # Discount rate entered as a percentage
+        fp = open(sys.argv[2], 'r')
+        # Discount rate entered as a percentage
+        d_rate = float(sys.argv[1])
     except:
         print("Usage: python main_DCF.py <DISCOUNT RATE> <CSV FILE>")
         sys.exit()
 
-    header = fp.readline().strip().split(',')                   # Generate header, a list containing the first two elems of the CSV file
+    # Generate header, a list containing the first two elems of the CSV file
+    header = fp.readline().strip().split(',')
     npv = 0                                                     # Net Present Value
     cashFlowList = []
 
@@ -50,21 +53,23 @@ def main():
         line = line.strip().split(',')
         cashFlowTemp = []
         for i in range(len(header)):
-            cashFlowTemp.append( int(line[i]) )
+            cashFlowTemp.append(int(line[i]))
         cashFlowList.append(cashFlowTemp)
         npv += presentVal(cashFlowTemp[1], d_rate, cashFlowTemp[0])
 
     npCF = np.array(cashFlowList)
 
     # Creating a bar chart showing the cash flows overtime
-    plt.title("Cash Flows over time: Given a NPV of %.2f and yearly rate of %.2f%%" % (npv, d_rate))
+    plt.title("Cash Flows over time: Given a NPV of %.2f and yearly rate of %.2f%%" % (
+        npv, d_rate))
     plt.xlabel('Years (0 = current year )')
     plt.ylabel('Cash flow ($)')
-    plt.bar(npCF[:,0], npCF[:,1], alpha=0.9, width=0.2 )
-    plt.savefig( 'cash_flow_fig.png' )
+    plt.bar(npCF[:, 0], npCF[:, 1], alpha=0.9, width=0.2)
+    plt.savefig('cash_flow_fig.png')
 
     plt.show()
     fp.close()
+
 
 if __name__ == "__main__":
     main()
