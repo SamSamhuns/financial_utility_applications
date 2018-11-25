92c55ad9640029a4b5e2f0f5280174d971a895a7
SamSamhuns <sthapuskar@yahoo.com>
2018-11-25 14:52:09 -0500

    Ran atom-beautify package for linting

--- Return and Volatilities/main_RAV.py
+++ Return and Volatilities/main_RAV.py
@@ -13,11 +13,12 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 
-def calc_summary_gen( eticker, annul_factor ):
+
+def calc_summary_gen(eticker, annul_factor):
     summary = eticker.describe()
 
-    annul_return = eticker.iloc[:,7].mean() * annul_factor
-    annul_volatility = eticker.iloc[:,7].std() * np.sqrt(annul_factor)
+    annul_return = eticker.iloc[:, 7].mean() * annul_factor
+    annul_volatility = eticker.iloc[:, 7].std() * np.sqrt(annul_factor)
     sharpe_ratio = 0
     # check if the optional risk free rate has been entered
     if len(sys.argv) == 3:
@@ -25,8 +26,8 @@ def calc_summary_gen( eticker, annul_factor ):
     else:
         sharpe_ratio = annul_return / annul_volatility
     # sys.argv[1] is the CSV file name
-    calc_output_fname = (sys.argv[1].split('.'))[0]+' calculated.csv'
-    summary_output_fname = (sys.argv[1].split('.'))[0]+' summary.csv'
+    calc_output_fname = (sys.argv[1].split('.'))[0] + ' calculated.csv'
+    summary_output_fname = (sys.argv[1].split('.'))[0] + ' summary.csv'
     eticker.to_csv(calc_output_fname)
     summary.to_csv(summary_output_fname)
 
@@ -39,53 +40,62 @@ def calc_summary_gen( eticker, annul_factor ):
         sum_writer.writerow(["Sharpe's Ratio", sharpe_ratio])
     sfile.close()
 
-def monthly_returns( eticker ):
-    eticker['Monthly Returns'] = eticker['Adj Close'].pct_change() * 100        # getting the monthly return and converting it a percent return
-    eticker['12 MMA'] = eticker['Monthly Returns'].rolling( window=12 ).mean()  # Get the 12 month moving average returns
+
+def monthly_returns(eticker):
+    # getting the monthly return and converting it a percent return
+    eticker['Monthly Returns'] = eticker['Adj Close'].pct_change() * 100
+    eticker['12 MMA'] = eticker['Monthly Returns'].rolling(
+        window=12).mean()  # Get the 12 month moving average returns
 
     # the annulaizetion factor for monthly returns is 12 (12 trading months in a year)
-    calc_summary_gen( eticker, 12)
+    calc_summary_gen(eticker, 12)
 
 
-def daily_returns( eticker ):
+def daily_returns(eticker):
     eticker['Daily Returns'] = eticker['Adj Close'].pct_change() * 100
-    eticker['10 DMA'] = eticker['Daily Returns'].rolling( window=10 ).mean()    # 10 day moving average
-    eticker['30 DMA'] = eticker['Daily Returns'].rolling( window=30 ).mean()    # 30 day moving average
+    eticker['10 DMA'] = eticker['Daily Returns'].rolling(
+        window=10).mean()    # 10 day moving average
+    eticker['30 DMA'] = eticker['Daily Returns'].rolling(
+        window=30).mean()    # 30 day moving average
 
     # the annulaizetion factor for monthly returns is 252 (252 trading days in a year)
-    calc_summary_gen( eticker, 252 )
+    calc_summary_gen(eticker, 252)
 
-def weekly_returns( eticker ):
+
+def weekly_returns(eticker):
     eticker['Weekly Returns'] = eticker['Adj Close'].pct_change() * 100
-    eticker['4 WMA'] = eticker['Weekly Returns'].rolling( window=4 ).mean()     # 4 weeks moving average
+    eticker['4 WMA'] = eticker['Weekly Returns'].rolling(
+        window=4).mean()     # 4 weeks moving average
 
     # the annulaizetion factor for weekly returns is 52 (52 trading weeks in a year)
-    calc_summary_gen( eticker, 52 )
+    calc_summary_gen(eticker, 52)
+
 
 def main():
     if len(sys.argv) != 2 and len(sys.argv) != 3:
         print('Usage main_RAV.py <yahoo_finance_returns_CSV> <Optional_risk_free_rate>')
         sys.exit()
-    ticker = pd.read_csv( sys.argv[1] )
+    ticker = pd.read_csv(sys.argv[1])
 
     # changing the 'Date' column to a pandas series datetime object and setting it as the df index
     ticker['Date'] = ticker['Date'].astype('datetime64[ns]')
-    ticker.set_index( ticker['Date'], inplace=True )
+    ticker.set_index(ticker['Date'], inplace=True)
     # Calcuting the time interval between sucessive data points
     dtime_interval = ticker.index[1] - ticker.index[0]
 
-    if dtime_interval.days in {1,3}:
-        #Data is on a daily basis
+    if dtime_interval.days in {1, 3}:
+        # Data is on a daily basis
         daily_returns(ticker)
     elif dtime_interval.days in {7}:
-        #Data is on a weekly basis
+        # Data is on a weekly basis
         weekly_returns(ticker)
-    elif dtime_interval.days in {28,29,30,31}:
-        #Data is on a monthly basis
+    elif dtime_interval.days in {28, 29, 30, 31}:
+        # Data is on a monthly basis
         monthly_returns(ticker)
     else:
         print("Data time intervals incorrect")
         sys.exit()
+
 
 if __name__ == '__main__':
     main()
