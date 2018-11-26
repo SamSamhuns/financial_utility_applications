3aacd72d1aba1bd1f5195f181c49ca56071ba8bf
SamSamhuns <sthapuskar@yahoo.com>
2018-11-26 12:50:34 -0500

    Added an interval arg, chks and reformatted mktime funcs

--- Return and Volatilities/security_csv.py
+++ Return and Volatilities/security_csv.py
@@ -28,35 +28,33 @@ def get_cookie_crumb():
         print('GET request unsuccessful: HTTP status', response.status_code)
         sys.exit()
 
-    alines = str(response.content)
-    cs = alines.find('CrumbStore')
-    cr = alines.find('crumb', cs + 10)
-    cl = alines.find(':', cr + 5)
-    q1 = alines.find('"', cl + 1)
-    q2 = alines.find('"', q1 + 1)
-    crumb = alines[q1 + 1:q2]
+    full_resp = str(response.content)
+    cs = full_resp.find('CrumbStore')
+    cr = full_resp.find('crumb', cs + 10)
+    cl = full_resp.find(':', cr + 5)
+    q1 = full_resp.find('"', cl + 1)
+    q2 = full_resp.find('"', q1 + 1)
+    crumb = full_resp[q1 + 1:q2]
 
     # print(crumb, cookie )
     return {'cookie': cookie, 'crumb': crumb}
 
 
-def get_sec_data(ticker, begindate, enddate, dtype):
+def get_sec_data(ticker, begindate, enddate, dtype, interval):
     cookie, crumb = None, None
     cc_result = get_cookie_crumb()
     cookie = cc_result['cookie']
     crumb = cc_result['crumb']
 
     # time.mktime() takes format ( tm_year, tm_month, tm_day, tm_hr, tm_min, tm_sec, tm_wday, tm_yday, tm_isdst)
-    tm_year, tm_month, tm_day, = (int(begindate[0:4]), int(
-        begindate[4:6]), int(begindate[6:8])
-    t_begin=time.mktime(tm_year, tm_month, tm_day, 4, 0, 0, 0, 0, 0))
-    t_end = time.mktime(tm_year, tm_month, tm_day, 18, 0, 0, 0, 0, 0))
+    t_begin = time.mktime((int(begindate[0:4]), int(begindate[4:6]), int(begindate[6:8]), 4, 0, 0, 0, 0, 0))
+    t_end = time.mktime((int(enddate[0:4]), int(enddate[4:6]), int(enddate[6:8]), 18, 0, 0, 0, 0, 0))
 
     # loading up the parameters for the historical data get request to yahoo finance
     param={}
     param['period1']=int(t_begin)
     param['period2']=int(t_end)
-    param['interval']='1d'
+    param['interval']='1mo'
     if dtype == 'quote':
         param['events']='history'
     elif dtype == 'dividend':
@@ -67,26 +65,25 @@ def get_sec_data(ticker, begindate, enddate, dtype):
 
     url='http://query1.finance.yahoo.com/v7/finance/download/{}?'.format(
         ticker)
-    resp2=session.get(url, params = param, cookies = cookie)
+    resp2=session.get(url, params=param, cookies=cookie)
 
     # Printing useful information
     # print(crumb, cookie)
+    # print( url, param )
     print(resp2.status_code)
     print(resp2.content)
 
 
 def main():
-    if len(sys.argv) != 5 and (sys.argv[4]).lower() != 'quote' and (sys.argv[4]).lower() != 'split' and (sys.argv[4]).lower() != 'dividend':
-        print("Usage:security.csv <TICKER> <START_DATE_YYYYMMDD> <END_DATE_YYYYMMDD> <DATA_quote/dividend/split>")
-        sys.quit()
+    if len(sys.argv) != 5 and (sys.argv[4]).lower() not in {'quote', 'split', 'dividend'} and (sys.argv[5] not in {'1d', '1wk', '1mo'}):
+        print("Usage:security.csv <TICKER> <START_DATE_YYYYMMDD> <END_DATE_YYYYMMDD> <DATA_quote/dividend/split> <interval_1d/1wk/1mo>")
+        sys.exit()
 
     ticker=(sys.argv[1]).upper()
-    start_date=int(sys.argv[2].replace(' ', '').replace('-', ''))
-    end_date=int(sys.argv[3].replace(' ', '').replace('-', ''))
+    start_date=sys.argv[2].replace(' ', '').replace('-', '')
+    end_date=sys.argv[3].replace(' ', '').replace('-', '')
     data_type=(sys.argv[4]).lower()
-    get_sec_data(ticker, start_date, end_date, data_type)
+    get_sec_data(ticker, start_date, end_date, data_type, interval)
 
-# FORMAT example for func args ( 'DJI', '20170515', '20170526', 'quote')
-
-
+# FORMAT example for func args ( 'DJI', '20170515', '20170526', 'quote', '1mo')
 main()
