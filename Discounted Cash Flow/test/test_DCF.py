b046ef91c2ef65c87eab6733593afa5cca9f901b
Samridha Man Shrestha <sms1198@PALL-3FL-05.PUBTERM.NYU.EDU>
2018-12-05 00:57:18 -0500

    Added a test folder and unittest test_DCF file

---
+++ Discounted Cash Flow/test/test_DCF.py
@@ -0,0 +1,15 @@
+import unittest 
+import sys
+sys.path.insert(0, '../')
+
+import main_DCF
+
+class TestDCF ( unittest.TestCase ):
+	def test_presentValue(self):
+		#pV = main_DCF.presentVal( futureVal ,rate, time ):
+		
+		self.assertAlmostEqual( 8065.096007, main_DCF.presentVal( 10808, 5, 6), places=6)
+		self.assertAlmostEqual( 9160.482332, main_DCF.presentVal( 10808.57, 2.5, 6.7), places=6)
+				
+if __name__ == "__main__":
+	unittest.main()
