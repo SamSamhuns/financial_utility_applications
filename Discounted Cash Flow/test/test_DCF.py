8a6c1b5fafc11192f5268c779e74fb9a0f5d68e2
Samridha_Shrestha <SamSamhuns@users.noreply.github.com>
2019-01-20 15:34:18 +0400

    Eliminated trailing whitespace

--- Discounted Cash Flow/test/test_DCF.py
+++ Discounted Cash Flow/test/test_DCF.py
@@ -1,4 +1,4 @@
-import unittest 
+import unittest
 import sys
 sys.path.insert(0, '../')
 
@@ -10,6 +10,6 @@ class TestDCF ( unittest.TestCase ):
 		
 		self.assertAlmostEqual( 8065.096007, main_DCF.presentVal( 10808, 5, 6), places=6)
 		self.assertAlmostEqual( 9160.482332, main_DCF.presentVal( 10808.57, 2.5, 6.7), places=6)
-				
+
 if __name__ == "__main__":
 	unittest.main()
