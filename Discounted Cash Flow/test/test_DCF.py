254a5b9091dcf30632f0d97aaedeaf754d847d4a
Samridha_Shrestha <SamSamhuns@users.noreply.github.com>
2019-02-06 21:21:42 +0400

    Removed trailing whitespace

--- Discounted Cash Flow/test/test_DCF.py
+++ Discounted Cash Flow/test/test_DCF.py
@@ -7,7 +7,6 @@ import main_DCF
 class TestDCF ( unittest.TestCase ):
 	def test_presentValue(self):
 		#pV = main_DCF.presentVal( futureVal ,rate, time ):
-		
 		self.assertAlmostEqual( 8065.096007, main_DCF.presentVal( 10808, 5, 6), places=6)
 		self.assertAlmostEqual( 9160.482332, main_DCF.presentVal( 10808.57, 2.5, 6.7), places=6)
 
